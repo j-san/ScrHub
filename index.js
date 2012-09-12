e3b3073779d5ad6dc9622e8e9e9fbd776814ab84
Jonathan Sanchez <jonathansanchez@me.com>
2012-09-12 23:54:58 +0200

    change redirect url

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.configure('dev', function () {
 app.configure('sta', function () {
     console.log('Using sta configuration');
     
-    process.host = "scrhub.herokuapp.com";
+    process.host = "www.scrhub.com";
     process.client_id = 'f48190b0a23185d38240';
     process.client_secret = '1fb743a81c15bd41836f02686bd529674d90de9c';
 });
