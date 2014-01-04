ca898d145ac702ef79cc9a508991adf6dc2a8e87
Jonathan Sanchez Pando <jonathansanchez@me.com>
2014-01-04 04:55:29 +0100

    reset proc...

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['ScrHub'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'a89f55690e390af7a4868cb798f00258d1974a21',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
